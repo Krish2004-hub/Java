887264a3de69972a999965b23c7490abd6a1791a
Krish2004-hub <kishorekris004@gmail.com>
2025-06-14 22:03:43 +0530

    Update Stockprofit.java

--- Stockprofit.java
+++ Stockprofit.java
@@ -6,20 +6,6 @@ class Main{
             arr[i]=s.nextInt();
         }
         Arrays.sort(arr);
-        //Array sorting manual logic...............>>
-        /* int temp=0;
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-        if(arr[j]>arr[j+1]){
-            temp=arr[j];
-          arr[j]=arr[j+1];
-           arr[j+1]=temp;
-        }
-    }
-}
- for(int i=0;i<n;i++){
-     System.out.println(arr[i]+" ");
- }    */
     }
     void minmax(int[]arr,Scanner s,int n){
         int profit=0;
