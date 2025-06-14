bd23ac9349464989c9dd1aab20f4768a76f178f5
Krish2004-hub <kishorekris004@gmail.com>
2025-06-14 22:06:30 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner s=new Scanner(System.in);
+       int n=s.nextInt();
+       int[]arr=new int[n];
+       for(int i=0;i<n;i++){
+           arr[i]=s.nextInt();
+    }
+    int temp=0;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            temp=arr[j];
+          arr[j]=arr[j+1];
+           arr[j+1]=temp;
+        }
+    }
+}
+
+ for(int i=0;i<n;i++){
+     System.out.println(arr[i]+" ");
+ }   
+}}
