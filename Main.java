661131ed02ba462b982e051e10431dc6cc4e8ac1
Krish2004-hub <kishorekris004@gmail.com>
2025-06-20 19:02:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,30 @@
 import java.util.*;
 class Main{
-    static int Reve(int n){
-        int dig=0;
-        int rev=0;
-        for(int i=1;i<=n;i++){
-            dig=n%10;
-            rev=dig*10+n;
+    void lucky(int n){
+        int sum=0;
+         if(n==7){
+            System.out.println("Lucky");
+            return;
+         }
+         while(n!=0){
+            int a=n%10;
+            sum=sum+a;
             n=n/10;
-        }
-        return rev;
-    }
-    static boolean prime(int n){
-        if(n<=1){
-            return false;
-        }
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i==0){
-                return false;
+            if(sum==7){
+                System.out.println("LUCKY");
+                return;
             }
-        }
-        return true;
+         }
+         System.out.println("Not lucky");
+         
+
     }
-    static boolean twistedPrime(int n){
-        int reversed=Reve(n);
-        return prime(n)&&prime(reversed);
-        }
-    
     public static void main(String[]args){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        if(twistedPrime(n)){
-            System.out.println(n+"TWISTED PRIME");
-        }
-        else{
-            System.out.println(n+ "NOT A TWISTED PRIME");
-        }
+        Main obj=new Main();
+        obj.lucky(n);
+
+
     }
 }
